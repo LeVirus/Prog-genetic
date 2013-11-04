3bb92165674d88743721cf2695aeb8fa8add6ef6
LeVirus <Charbonneau_Cyril@hotmail.fr>
2013-11-04 18:20:48 +0100

    ss

--- Header/Population.hpp
+++ Header/Population.hpp
@@ -1,14 +1,18 @@
 #ifndef POP
 #define POP
 
-class Individu;
+#include <vector>
+#include "Individu.hpp"
+
 
 class Population{
   private:
-	 Individu *tabIndividu;
+	 std::vector<Individu> tabIndividu;
+	 std::vector<Individu>::iterator itTabInd;
 	 unsigned short nbrIndividu, nbrVille;
   public:
 	 Population(unsigned short nombreVille, unsigned short nombreIndividu);
+	 std::vector<Individu> vectorInd();
 	 void afficherPop();
 	 ~Population();
 };
