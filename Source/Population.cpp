a38ceb31538187d89ae93a671cf2a122aa4d2a06
LeVirus <Charbonneau_Cyril@hotmail.fr>
2013-11-04 18:20:22 +0100

    ss

--- Source/Population.cpp
+++ Source/Population.cpp
@@ -7,22 +7,25 @@ using namespace std;
 Population::Population(unsigned short nombreVille, unsigned short nombreIndividu){
 	nbrVille=nombreVille; 
 	nbrIndividu=nombreIndividu;
-	tabIndividu=new Individu[nbrIndividu];
-	for(unsigned short i=0;i<nbrIndividu;i++){
-		tabIndividu[i].initInd(nbrVille);
+	tabIndividu.resize(nbrIndividu);
+	for(itTabInd=tabIndividu.begin();itTabInd<tabIndividu.end();itTabInd++){
+		(*itTabInd).initInd(nbrVille);
 	 	//tabIndividu[i].afficherInd();
 	}
 	
 }
 
+
+std::vector<Individu> Population::vectorInd(){
+	return tabIndividu;
+}
+
 void Population::afficherPop(){
-	if(!tabIndividu)return;
-	for(unsigned short i=0;i<nbrIndividu;i++){
-	 	tabIndividu[i].afficherInd();
+	for(itTabInd=tabIndividu.begin();itTabInd<tabIndividu.end();itTabInd++){
+	 	(*itTabInd).afficherInd();
 	}
 }
 
 Population::~Population(){
-	if(tabIndividu)delete [] tabIndividu;
 }
 
