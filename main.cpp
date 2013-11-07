5e37117832cf4b98bc4cea077c82065a9ae80619
LeVirus <Charbonneau_Cyril@hotmail.fr>
2013-11-07 18:06:31 +0100

    main

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "Racine.hpp"
+
+using namespace std;
+
+class Carte;
+class Population;
+
+Carte *carteG;
+Population *popG;
+
+int main(){
+  Racine a;
+  a.lancerProg();
+  return 0;
+}
